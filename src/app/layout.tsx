110fbcf6d623f6c527e6e50f9d61501a938f4d50
Pat Zdziabek <159633087+PatZdz@users.noreply.github.com>
2025-06-13 18:46:09 +0200

    ready

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="pl">
       <body className={inter.className}>
         <main>{children}</main>
         <CookieConsent />
